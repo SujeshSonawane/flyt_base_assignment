b8914a6ae72bb57e84e6c6d4d38e641aa4b1bf6c
SujeshSonawane <162686374+SujeshSonawane@users.noreply.github.com>
2024-03-09 15:52:57 +0530

    Update demoapp1.cpp

--- demoapp1.cpp
+++ demoapp1.cpp
@@ -4,7 +4,7 @@ from flyt_python import api
 drone = api.navigation(timeout=120000) # instance of flyt droneigation class
 # at least 3sec sleep time for the drone interface to initialize properly
 time.sleep(3)
-print ' drone taking off at hight of 5 m at point A of square ABCD'
+print ' drone taking off at hight of 5 m at origin point A of square ABCD'
 drone.take_off(5.0)
 print ' moving towards point B at side length of 6.5 m of square ABCD'
 drone.position_set(6.5, 0, 0, relative=True)
