c0e369af0bb5a120ff4130dbce48e33cd0cb70a9
SujeshSonawane <162686374+SujeshSonawane@users.noreply.github.com>
2024-03-09 15:22:27 +0530

    Update demoapp1.cpp

--- demoapp1.cpp
+++ demoapp1.cpp
@@ -1,13 +1,20 @@
-#include <cpp_api/navigation_bridge.h>
-#include <iostream>
-
-Navigation nav;
-int main(int argc, char *argv[])
-{
-    nav.take_off(3.0);						//Taking Off
-    nav.position_set(5,0,-3);				//Sending Position Setpoints
-    nav.position_set(5,5,-3);
-    nav.position_set(0,5,-3);
-    nav.position_set(0,0,-3);
-    nav.land(false);									//Landing
-}
+#!/usr/bin/env python
+import time
+from flyt_python import api
+drone = api.navigation(timeout=120000) # instance of flyt droneigation class
+# at least 3sec sleep time for the drone interface to initialize properly
+time.sleep(3)
+print ' drone taking off at hight of 5 m at point A of square ABCD'
+drone.take_off(5.0)
+print ' moving towards point B at side length of 6.5 m of square ABCD'
+drone.position_set(6.5, 0, 0, relative=True)
+print ' moving towards point c at side length of 6.5 m of square ABCD'
+drone.position_set(0, 6.5, 0, relative=True)
+print ' moving towards point D at side length of 6.5 m of square ABCD'
+drone.position_set(-6.5, 0, 0, relative=True)
+print ' moving towards origin point A at side length of 6.5 m of square ABCD'
+drone.position_set(0, -6.5, 0, relative=True)
+print 'Landing'
+drone.land(async=False)
+# shutdown the instance
+drone.disconnect()
